ebd2c585cb7866d9016ba5ae5b37052c3e44d149
Michael Patterson <map222@gmail.com>
2015-01-13 23:11:09 -0800

    Started results calculation function
    Fought with pandas to figure out how to iterate through all rows, and
    how to count stuff.

--- pytchFx/helpers.py
+++ pytchFx/helpers.py
@@ -5,6 +5,7 @@ Created on Thu Jan 08 20:45:46 2015
 @author: Michael Patterson
 """
 import pdb
+import pandas as pd
 
 def get_pitches_from_template(tp, ap, sp_var = 1, px_var = 0.15, pz_var = 0.15,
                               x0_var = 0.5, z0_var = 0.5, br_angle_var = 10 ):
@@ -31,3 +32,32 @@ def get_pitches_from_template(tp, ap, sp_var = 1, px_var = 0.15, pz_var = 0.15,
        & (ap['z0'] > tp_z0 - z0_var) & (ap['z0'] < tp_z0 + z0_var) 
        & (ap['break_angle'] > tp_br - br_angle_var) & (ap['break_angle'] < tp_br + br_angle_var) ]
     return new_pitches
+    
+    # do I want to do merge outside this?
+def calc_results_from_pitches(sample_pitches, pitch_abs):
+    """ calculates the balls, strikes, ..., from a sample data frame containing one or more pitches """
+    
+    pitch_info = ['px', 'pz', 'x0', 'z0', 'break_angle', 'end_speed']
+    results_info = ['num', 'gameday_link', 'des', 'event']
+
+    # get the pitches of the sample pitches
+    sim_pitches = pd.DataFrame(columns = pitch_abs.columns.values.tolist())
+    
+    pdb.set_trace()
+    for i, rows in sample_pitches.iterrows():
+        sim_pitches = sim_pitches.append(get_pitches_from_template(rows, pitch_abs))
+        
+    # count the number of results for each pitch
+    sample_results = sample_pitches.groupby('des').size() / len(sample_pitches)
+    sim_pitches = sim_pitches.groupby('des').size() / len(sim_pitches)
+    
+#def get_subset_pitches(pitch_abs_in):
+    
+def calc_results_pitches(pitch_abs_in):
+    
+            
+def merge_pitch_ab(pitches_in, abs_in):
+    """ merge a pitch and atbat dataframe """
+    # create merged db with just information about pitch, and results it got    
+    return pd.merge(pitches_in, abs_in, how = 'inner', on=['num', 'gameday_link'])
+    
--- pytchFx/io.py
+++ pytchFx/io.py
@@ -7,16 +7,40 @@ Created on Sat Jan 10 10:22:19 2015
 
 import pandas as pd
 import sqlite3
+import pdb
 
 def load_pitches(year, row_limit = 1000000 ):
+    """ Loads pitch database from sqlite file in directory
+        
+    year: integer  for year
+    
+    row_limit" is in case you want to limit how much info you get
+    """
+    
+    pdb.set_trace()
+    
     # open connection to sqlite database
     filename = 'pitchFX_' + str(year) + '.sqlite3'
-    db = sqlite3.connect("pitchFx_2013.sqlite3")
+    db = sqlite3.connect(filename)
     
     # load the pitches in
-    pitches_out = pd.read_sql('SELECT * FROM pitch LIMIT' + str(row_limit), db)
+    pitches_out = pd.read_sql('SELECT * FROM pitch LIMIT ' + str(row_limit), db)
     
     # fix the datatype of the break from text to int
     pitches_out['break_angle'] = pitches_out['break_angle'].astype(float)
     pitches_out['break_y'] = pitches_out['break_y'].astype(float)
     pitches_out['break_length'] = pitches_out['break_length'].astype(float)
+    
+    return pitches_out
+    
+def load_abs(year, row_limit = 1000000):
+    """ loads atbat info from sqlite database stored in same directory """
+    
+    # open connection to sqlite database
+    filename = 'pitchFX_' + str(year) + '.sqlite3'
+    db = sqlite3.connect(filename)
+    
+    # load the pitches in
+    abs_out = pd.read_sql('SELECT * FROM atbat LIMIT ' + str(row_limit), db)
+    
+    return abs_out
