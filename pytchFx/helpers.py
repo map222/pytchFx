cc22a83a1dd4f99719c6381de50a14737164f744
Michael Patterson <map222@gmail.com>
2015-03-04 22:11:58 -0800

    Combined results
    Combined results for rare events into common events; e.g. Foul Tips
    become simple Fouls. Also, verified that my Ball/Call strike problem is
    due to edge of strikezone.

--- pytchFx/helpers.py
+++ pytchFx/helpers.py
@@ -47,6 +47,9 @@ def get_pitches_from_templates(template_pitches, all_pitches):
     # go through all template pitches, and add new data (need append because can be multiple pitches)
     for i, row in template_pitches.iterrows():
         resampled_pitches = resampled_pitches.append(get_pitches_from_template(row, all_pitches))
+    # the dynamic addition works fine for small samples, but as you go beyond say 200 pitches, it nonlinearly slows thing down
+        # time / pitch for 100 pitches = 0.08sec
+        # time / pitch for 2000 pitches = 0.27 sec
         
     return resampled_pitches
     
@@ -54,23 +57,34 @@ def calc_results_from_pitches(pitch_abs):
     """ calculates results from a pitch_ab dataframe
         first it counts numbers of balls, strikes, etc
         then normalizes per pitch """
-    
-    pitch_info = ['px', 'pz', 'x0', 'z0', 'break_angle', 'end_speed']
-    results_info = ['num', 'gameday_link', 'des', 'event']
 
-    #pdb.set_trace()
     # count the number of results for each pitch
     des_results = pitch_abs.groupby('des').size() / len(pitch_abs)
-    
-    return des_results
+    balls = des_results[['Ball', 'Ball In Dirt', 'Intent Ball', 'Pitchout']].sum()
+    fouls = des_results[['Foul', 'Foul (Runner Going)', 'Foul Tip', 'Foul Bunt']].sum()
+    swing_strikes = des_results[['Swinging Strike', 'Missed Bunt', 'Swinging Strike (Blocked)']].sum()
+    #pdb.set_trace()
+    kept_results = des_results[['In play, no out', 'In play, out(s)', 'In play, run(s)', 'Called Strike', 'Hit By Pitch']]
+        
+    return kept_results.append(pd.Series([balls, fouls, swing_strikes], ['Ball', 'Foul', 'Swinging Strike']))
     
 #def get_subset_pitches(pitch_abs_in):
-    
-def calc_results_pitches(pitch_abs_in):
-    print 'empty'
             
 def merge_pitch_ab(pitches_in, abs_in):
     """ merge a pitch and atbat dataframe """
     # create merged db with just information about pitch, and results it got    
     return pd.merge(pitches_in, abs_in, how = 'inner', on=['num', 'gameday_link'])
-    
+
+# this way of cleaning results in the database is super slow
+# better to just do it afterwards
+def clean_results(pitch_abs):
+    """ Group all of the rare events into more common descriptors """
+    pitch_abs.replace('Ball In Dirt', 'Ball', inplace = True)
+    pitch_abs.replace('Foul (Runner Going)', 'Foul', inplace = True)
+    pitch_abs.replace('Foul Tip', 'Foul', inplace = True)
+    pitch_abs.replace('Foul Bunt', 'Foul', inplace = True)
+    pitch_abs.replace('Intent Ball', 'Ball', inplace = True)
+    pitch_abs.replace('Pitchout', 'Ball', inplace = True)
+    pitch_abs.replace('Missed Bunt', 'Swinging Strike', inplace = True)
+    pitch_abs.replace('Swinging Strike (Blocked)', 'Swinging Strike', inplace = True)
+    return pitch_abs
